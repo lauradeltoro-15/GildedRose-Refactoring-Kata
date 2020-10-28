5fbbf28c9d6e49571cc4f341a2b00d93512ca4c1
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-10-28 21:39:08 +0000

    Refactoring kata

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,12 +1,21 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { GildedRose } from '../app/gilded-rose';
+import { items } from './golden-master-text-test';
+
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+    it('should foo', function () {
+        const gildedRose = new GildedRose();
+        var days: number = 4;
+        for (let i = 0; i < days; i++) {
+            console.log("-------- day " + i + " --------");
+            console.log("name, sellIn, quality");
+            items.forEach(element => {
+                console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
+            });
+            console.log();
+            gildedRose.updateQuality(items);
+        }
     });
 
 });
